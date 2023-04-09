216526767955d6ca75d248c4f8bf3cb7d04c1eae
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-04-09 12:33:00 -0600

    Update format of files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import EmojiList from "./emojiList.json"
+import EmojiList from "./emojiList.json";
 import Header from "./components/Header/Header";
 import SearchInput from "./components/SearchInput/SearchInput";
 import EmojiResults from "./components/EmojiResults/EmojiResults";
-import './App.css';
+import "./App.css";
 
 function App() {
   const emojiListArr = Object.values(EmojiList);
@@ -11,22 +11,24 @@ function App() {
   const [results, setResults] = useState(emojiListArr.slice(0, 20));
 
   function findResults(term) {
-    return emojiListArr.filter(emoji => emoji.keywords.includes(term)).slice(0, 20);
+    return emojiListArr
+      .filter((emoji) => emoji.keywords.includes(term))
+      .slice(0, 20);
   }
 
   const searchEmojiList = (e) => {
-    const term = e.target.value
-    const results = findResults(term.toLowerCase()); 
+    const term = e.target.value;
+    const results = findResults(term.toLowerCase());
 
     setSearchTerm(term);
     setResults(results);
-  }
+  };
 
   return (
     <div className="App">
       <Header emojiList={emojiListArr} />
       <SearchInput searchTerm={searchTerm} update={searchEmojiList} />
-      <EmojiResults results={results} />   
+      <EmojiResults results={results} />
     </div>
   );
 }
--- src/components/EmojiResults/EmojiRow.jsx
+++ src/components/EmojiResults/EmojiRow.jsx
@@ -1,10 +1,10 @@
 const EmojiRow = ({ title, symbol }) => {
-    return(
-        <div id="EmojiRow">
-            <span className="emoji-row symbol">{symbol}</span>
-            <span className="emoji-row title">{title}</span>
-        </div>
-    );
+  return (
+    <div id="EmojiRow">
+      <span className="emoji-row symbol">{symbol}</span>
+      <span className="emoji-row title">{title}</span>
+    </div>
+  );
 };
 
 export default EmojiRow;
