f2f559cd5436b0cb49ff8e097b6c927e1f412ba9
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-04-09 12:26:39 -0600

    Add in functionality for results update

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ import SearchInput from "./components/SearchInput/SearchInput";
 import EmojiResults from "./components/EmojiResults/EmojiResults";
 import './App.css';
 
-
 function App() {
   const emojiListArr = Object.values(EmojiList);
   const [searchTerm, setSearchTerm] = useState("");
   const [results, setResults] = useState(emojiListArr.slice(0, 20));
 
+  function findResults(term) {
+    const newResults = emojiListArr.filter(emoji => emoji.keywords.includes(term));
+    return newResults;
+  }
+
   const updateSearchTerm = (e) => {
-    setSearchTerm(e.target.value);
+    const term = e.target.value
+    const results = findResults(term.toLowerCase()); 
+
+    setSearchTerm(term);
+    setResults(results);
   }
 
   return (
--- src/components/EmojiResults/EmojiResults.jsx
+++ src/components/EmojiResults/EmojiResults.jsx
@@ -1,17 +1,14 @@
 import EmojiRow from "./EmojiRow";
 
 const EmojiResults = ({ results }) => {
-    console.log(results);
-    return(
-        <div id="EmojiResults">
-            {results.map(emoji => {
-                const { title, symbol } = emoji;
-                return(
-                    <EmojiRow title={title} symbol={symbol}/>
-                )
-            })}
-        </div>
-    );
-}
+  return (
+    <div id="EmojiResults">
+      {results.map((emoji) => {
+        const { title, symbol } = emoji;
+        return <EmojiRow title={title} symbol={symbol} />;
+      })}
+    </div>
+  );
+};
 
 export default EmojiResults;
